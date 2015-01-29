177edb4bc113610c8cb3f4e30cb4d4bea03dbd5a
Darin Minamoto <darinm223@gmail.com>
2015-01-28 18:22:50 -0800

    Added some extra sql fields

--- create.sql
+++ create.sql
@@ -1,7 +1,7 @@
 /* SQL statements to create the tables necessary for the application */
 
 CREATE TABLE Bidder (
-  UserID INTEGER,
+  UserID VARCHAR(80),
   Rating INTEGER,
   Location VARCHAR(80),
   Country VARCHAR(80),
@@ -10,7 +10,7 @@ CREATE TABLE Bidder (
 );
 
 CREATE TABLE Seller (
-  UserID INTEGER,
+  UserID VARCHAR(80),
   Rating INTEGER,
 
   PRIMARY KEY (UserID)
@@ -19,27 +19,28 @@ CREATE TABLE Seller (
 CREATE TABLE ItemDescription (
   ItemID INTEGER,
   Name VARCHAR(80),
-  Buy_Price FLOAT,
-  First_Bid,
+  Buy_Price DECIMAL(10, 2),
+  First_Bid DECIMAL(10, 2),
   Number_of_Bids INTEGER,
   Location VARCHAR(80),
-  Latitude,
-  Longitude,
+  Latitude DECIMAL(9, 6),
+  Longitude DECIMAL(9, 6),
+  Currently DECIMAL(10, 2),
   Country VARCHAR(80),
-  Started,
-  Ends,
-  Seller INTEGER,
-  Description VARCHAR(80),
+  Started DATETIME,
+  Ends DATETIME,
+  Seller VARCHAR(80),
+  Description VARCHAR(4000),
 
-  PRIMARY KEY (ItemID)
+  PRIMARY KEY (ItemID),
   FOREIGN KEY Seller REFERENCES Seller(UserID)
 );
 
 CREATE TABLE Bids (
   ItemID INTEGER,
-  UserID INTEGER,
-  Time INTEGER, /* WTF is time */
-  Amount INTEGER,
+  UserID VARCHAR(80),
+  Time DATETIME,
+  Amount DECIMAL(10, 2),
 
   FOREIGN KEY (ItemID) REFERENCES ItemDescription(ItemID),
   FOREIGN KEY (UserID) REFERENCES Bidder(UserID)

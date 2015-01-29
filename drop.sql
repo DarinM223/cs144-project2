a0daf88fbc98575aa5f2b8cdf79ff329f602b0d9
Darin Minamoto <darinm223@gmail.com>
2015-01-29 03:11:05 -0800

    Fixed dropping without breaking constraints

--- drop.sql
+++ drop.sql
@@ -1,7 +1,7 @@
 /* SQL statements to drop the tables necessary for the application */
 
+DROP TABLE IF EXISTS ItemCategory;
+DROP TABLE IF EXISTS Item;
 DROP TABLE IF EXISTS Bidder;
 DROP TABLE IF EXISTS Seller;
 DROP TABLE IF EXISTS Bid;
-DROP TABLE IF EXISTS Item;
-DROP TABLE IF EXISTS ItemCategory;
